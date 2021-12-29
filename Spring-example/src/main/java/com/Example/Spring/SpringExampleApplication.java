c33798fc713936a2a81da5f99172edb724fd921a
brahminiboina <96646057+brahminiboina@users.noreply.github.com>
2021-12-29 14:34:52 +0530

    Add files via upload

---
+++ Spring-example/src/main/java/com/Example/Spring/SpringExampleApplication.java
@@ -0,0 +1,15 @@
+package com.Example.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@SpringBootApplication
+public class SpringExampleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringExampleApplication.class, args);
+	}
+
+}
